2e86af6ac7471ad1d0295da95e39645fa896c3d2
Sensaku <barbe.arnaud@hotmail.fr>
2022-08-30 17:16:36 +0200

    Correction: wrong script send

--- Script/transform_csv.py
+++ Script/transform_csv.py
@@ -3,6 +3,8 @@ import glob
 import pandas as pd
 import numpy as np
 import hashlib
+import json
+import time
 
 NAMESPACE = "http://www.urgi.versailles.inrae.fr/"
 FRANCE_URI = "http://wikidata/org/entity/Q142"
@@ -19,10 +21,11 @@ URI = {
 variable_not_found = set()
 
 
-trial_label = ["URI", "TrialName", "TrialCode", "BeginningDate", "EndingDate", "TrialCampaign", "Institution"]
-biological_label = ["URI", "Taxon", "Species", "AccessionNumber", "AccessionName", "LotName"]
-observation_unit_label = ["URI", "BiologicalMaterial", "itk", "trial", "trial_name", "lot_value", "itk_value", "observation_level"]
-observation_label = ["URI", "observation_unit", "variable", "value", "trait", "method", "scale"]
+studies_label = ["study_id", "study_name", "start_date", "end_date", "description"]
+biological_label = ["biological_id", "genus", "species", "subspecies", "accession_number", "accession_name", "material_source_id", "material_source_doi"]
+observation_unit_label = ["id", "study", "biological", "observation_level", "observation_level_number", "factor_mode", "factor_value"]
+observation_label = ["observation_unit", "observation_id", "variable", "trait","method", "scale", "value"]
+
 factor_label = ["uri_factor", "uri_factor_value", "factor", "factor_value", "factor_name", "description"]
 person_label = ["type", "name", "email", "institution"]
 gps_label = ["study", "site", "latitude", "longitude", "altitude"]
@@ -51,6 +54,13 @@ def extract_variable_dict(dataframe):
 
     return variable_co, trait_co, method_co, scale_co
 
+def extract_factor(json_array):
+    dict_value = eval(json_array)[0]
+    return dict_value["factor"], dict_value["modality"]
+
+def extract_observation(json_array):
+    dict_value = eval(json_array)
+    return dict_value
 
 def co_321_uri_maker(uri):
     return f"http://www.cropontology.org/rdf/CO_321/{uri.split(':')[1]}"
@@ -59,7 +69,7 @@ def co_321_uri_maker(uri):
 def encoding_resolve(string):
     return string.replace("Ã©", "é")
 
-def uri_hash_maker(to_hash, ns):
+def uri_hash_maker(to_hash):
     """
     Create an URI with the namespace and the associated id as hash
     :param to_hash:string
@@ -77,46 +87,46 @@ def study_maker(dataframe):
     """
     NOTE: THIS IS THE STUDY!!
     Create data for trial CSV
-    The columns label are ["URI", "TrialName", "TrialCode", "BeginningDate", "EndingDate", "TrialCampaign", "Institution"]
+    The columns label are ["study_id", "study_name", "start_date", "end_date", "description"]
     :param dataframe:DataFrame
     :return: data:List
     """
-    global trial_name
-    data = list()
-    trial_name = dataframe["Trial"]["TrialName"].values[0]
-    data.append(uri_hash_maker(dataframe["Trial"]["TrialName"].values[0], URI["study"]))
-    #data.append(f"{URI['trial']}{trial_name}")
-    data.append(dataframe["Trial"]["TrialName"].values[0].strip())
-    data.append(dataframe["Trial"]["TrialCode"].values[0].strip())
-    data.append(dataframe["Trial"]["BeginningDate"].values[0].strip())
-    data.append(dataframe["Trial"]["EndingDate"].values[0].strip())
-    data.append(dataframe["Trial"]["TrialSet"].values[0].strip())
-    data.append(dataframe["Contact"]["Institution"].values[0].strip())
-    return data
+    list_data = list()
+    for row_elt in dataframe.index:
+
+        data = list()
+
+        data.append(uri_hash_maker(dataframe["study_unique_id"][row_elt]))
+        data.append(dataframe["study_title"][row_elt])
+        data.append(dataframe["start_date_of_study"][row_elt])
+        data.append(dataframe["end_date_of_study"][row_elt])
+        data.append(dataframe["description_of_growth_facilty"][row_elt])
+
+        list_data.append(data)
+
+    return list_data
 
 
 def biological_maker(dataframe):
     """
     Create data for biological material CSV
-    The column labels are ["URI", "Taxon", "Species", "AccessionNumber", "AccessionName", "LotName"]
+    The column labels are ["biological_id", "genus", "species", "subspecies", "accession_number", "accession_name", "material_source_id", "material_source_doi"]
     :param dataframe:DataFrale
     :return: data:List
     """
     list_data = list()
-    #dataframe.drop(['HoldingInstitution', 'CollectionName', 'CollectionType', 'PanelName', 'PanelSize'], axis=1, inplace=True)
 
     for row_elt in dataframe.index:
         data = list()
 
-        number, name, lot = dataframe['AccessionNumber'][row_elt],\
-                            dataframe['AccessionName'][row_elt],\
-                            dataframe['LotName'][row_elt]
-        data.append(uri_hash_maker(f"{lot}", URI["biological"]))
-        data.append(" ".join(dataframe["TaxonScientificName"][row_elt].split(" ")[0:2]))
-        data.append(dataframe["TaxonScientificName"][row_elt].split(" ")[2])
-        data.append(number)
-        data.append(name)
-        data.append(lot)
+        data.append(dataframe["biological_material_id"][row_elt])
+        data.append(dataframe["genus"][row_elt])
+        data.append(dataframe["species"][row_elt])
+        data.append(dataframe["subsp"][row_elt])
+        data.append(dataframe["accession_number"][row_elt])
+        data.append(dataframe["accession_name"][row_elt])
+        data.append(dataframe["material_source_id"][row_elt])
+        data.append(dataframe["material_source_doi"][row_elt])
 
         list_data.append(data)
 
@@ -126,7 +136,7 @@ def biological_maker(dataframe):
 def observation_unit_maker(dataframe):
     """
     Create data for observation unit CSV
-    The column labels are ["URI", "BiologicalMaterial", "itk"]
+    The column labels are ["id", "study", "biological", "observation_level", "observation_level_number","factor_mode", "factor_value"]
     :param dataframe:
     :return:
     """
@@ -134,24 +144,24 @@ def observation_unit_maker(dataframe):
     
     for row_elt in dataframe.index:
         data = list()
-        lot = dataframe['LotName'][row_elt]
-        rep = np.int64(0)
 
-        if 'itk' in dataframe.columns:
-            itk = dataframe['itk'][row_elt]
+        data.append(dataframe["observationunitdbid"][row_elt])
+        data.append(uri_hash_maker(dataframe["studydbid"][row_elt]))
+        data.append(dataframe["germplasmname"][row_elt])
+        data.append(dataframe["observationlevel"][row_elt])
+
+        if pd.isnull(dataframe["replicate"][row_elt]):
+            data.append(0)
         else:
-            itk = "no-itk"
-        if 'Rep' in dataframe.columns:
-            rep = dataframe['Rep'][row_elt]
+            data.append(dataframe["replicate"][row_elt])
 
-        data.append(uri_hash_maker(f"{trial_name}{lot}{itk}{rep}", URI["observation_unit"]))
-        data.append(uri_hash_maker(f"{lot}", URI["biological"]))
-        data.append(uri_hash_maker(f"{itk}", URI["factor_value"]))
-        data.append(uri_hash_maker(f"{trial_name}", URI["study"]))
-        data.append(trial_name)
-        data.append(lot)
-        data.append(itk)
-        data.append(f"{rep}")
+        if dataframe["experimental_factors"][row_elt] == "[]":
+            data.append("itk")
+            data.append("non traité")
+        else:
+            factor_mode, factor_value = extract_factor(dataframe["experimental_factors"][row_elt])
+            data.append(factor_mode)
+            data.append(factor_value)
 
         list_data.append(data)
 
@@ -181,60 +191,32 @@ def factor_maker(dataframe):
     return list_data
         
 
-def observation_maker(dataframe, var_df):
+def observation_maker(dataframe):
     """
     Create data for observation CSV
     The column labels are
     One row per (variable, value)
+    The column labels are ["observation_unit", "observation_id", "variable", "trait","method", "scale", "value"]
     :param dataframe:
     :return:
     """
-
-    var_transform = dataframe.iloc[:, dataframe.columns.get_loc("Y(m)")+1:]
-    var_transform = var_transform[var_transform.columns.drop(list(var_transform.filter(regex='date')))]
     list_data = list()
+
     for row_elt in dataframe.index:
+            observations = extract_observation(dataframe["observations"][row_elt])
 
-        lot = dataframe["LotName"][row_elt]
-        rep = "0"
-        if 'itk' in dataframe.columns:
-            itk = dataframe["itk"][row_elt]
-        else:
-            itk = "no-itk"
-        if "Rep" in dataframe.columns:
-            rep = dataframe["Rep"][row_elt]
-        for col in var_transform.columns:
-            data = list()
-            variable_shortname = col.strip().split("_")[0]
-            search_variable = var_df[var_df["VariableShortName"] == variable_shortname]["VariableID"]
-            if search_variable.empty:
-                if variable_shortname in co_321_variables.keys():
-                    val = co_321_variables[variable_shortname]
-                    variable = val
-                    traits = co_321_trait[val]
-                    methods = co_321_method[val]
-                    scale = co_321_scale[val]
-                else:
-                    variable_not_found.add(variable_shortname)
-                    continue
-            else:
-                val = search_variable.values[0]
-                variable = val
-                traits = co_321_trait[val]
-                methods = co_321_method[val]
-                scale = co_321_scale[val]
+            for obs in observations:
+                data = list()
 
-            if pd.isnull(dataframe[col][row_elt]):
-                continue
-            data.append(uri_hash_maker(f"{trial_name}{lot}{itk}{rep}{variable}", URI["observation"]))
-            data.append(uri_hash_maker(f"{trial_name}{lot}{itk}{rep}", URI["observation_unit"]))
-            data.append(co_321_uri_maker(variable))
-            data.append(dataframe[col][row_elt])
-            data.append(co_321_uri_maker(traits))
-            data.append(co_321_uri_maker(methods))
-            data.append(co_321_uri_maker(scale))
-
-            list_data.append(data)
+                obs_variable = obs["observationVariableDbId"]
+                data.append(dataframe["observationunitdbid"][row_elt])
+                data.append(obs["observationDbId"])
+                data.append(co_321_uri_maker(obs_variable))
+                data.append(co_321_uri_maker(co_321_trait[obs_variable]))
+                data.append(co_321_uri_maker(co_321_method[obs_variable]))
+                data.append(co_321_uri_maker(co_321_scale[obs_variable]))
+                data.append(obs["value"])
+                list_data.append(data)
 
     return list_data
 
@@ -249,7 +231,7 @@ def gps_maker(dataframe):
 
     for row_elt in dataframe.index:
         data = list()
-        data.append(uri_hash_maker(dataframe["study_title"][row_elt], ""))
+        data.append(uri_hash_maker(dataframe["study_title"][row_elt]))
         data.append(dataframe["experimental_site_name"][row_elt])
         data.append(dataframe["geographic_location_lattitude"][row_elt])
         data.append(dataframe["geographic_location_longitude"][row_elt])
@@ -284,10 +266,10 @@ def person_maker(dataframe):
 
 
 if __name__ == '__main__':
-    cc = True
+    alpha = time.time()
     current_directory = os.getcwd()
     co_321_df = pd.read_csv("CO_321-Wheat Crop Ontology.csv", sep=";")
-    co_321_variables, co_321_trait, co_321_method, co_321_scale = extract_variable_dict(co_321_df)
+    co_321_variable, co_321_trait, co_321_method, co_321_scale = extract_variable_dict(co_321_df)
 
     print(current_directory + "\\wheat_complete")
     os.chdir(f"{current_directory}\\wheat_complete")
@@ -296,12 +278,12 @@ if __name__ == '__main__':
 
     studies_df = pd.read_csv("studies.csv").iloc[:, 1:]
     investigation_df = pd.read_csv("investigation.csv").iloc[:, 1:]
+    biological_df = pd.read_csv("biological_material.csv").iloc[:, 1:]
 
-    gps_data = gps_maker(studies_df)
-    print(gps_data)
+    gps_out = pd.DataFrame(gps_maker(studies_df), columns=gps_label)
+    studies_out = pd.DataFrame(study_maker(studies_df), columns=studies_label)
+    biological_out = pd.DataFrame(biological_maker(biological_df), columns=biological_label)
 
-    trial_data = list()
-    biological_data = list()
     observation_unit_data = list()
     observation_data = list()
     factor_data = list()
@@ -309,26 +291,29 @@ if __name__ == '__main__':
 
     for file in file_csv:
         df = pd.read_csv(file).iloc[:, 1:]
-
-        trial_data.append(study_maker(df))
-        biological_data.extend(biological_maker(df["PlantMaterial"]))
-        observation_unit_data.extend(observation_unit_maker(df["Data"]))
-        observation_data.extend(observation_maker(df["Data"], df["Variable"]))
-        factor_data.extend(factor_maker(df["Treatment"]))
-        person_data.extend(person_maker(df["Contact"]))
+        observation_unit_data.extend(observation_unit_maker(df))
+        observation_data.extend(observation_maker(df))
 
     os.chdir(f"{current_directory}")
+observation_unit_df = pd.DataFrame(observation_unit_data, columns=observation_unit_label)
+observation_df = pd.DataFrame(observation_data, columns=observation_label)
 
-    study_df = pd.DataFrame(trial_data, columns=trial_label)
-    biological_df = pd.DataFrame(biological_data, columns=biological_label)
+studies_out.to_csv("studies.csv", index=False, encoding="utf-8")
+gps_out.to_csv("gps.csv", index=False, encoding="utf-8")
+biological_out.to_csv("biological.csv", index=False, encoding="utf-8")
+observation_unit_df.to_csv("observation_unit.csv", index=False, encoding="utf-8")
+observation_df.to_csv("observations.csv", index=False, encoding="utf-8")
+
+beta = time.time()
+print(beta - alpha)
+
+"""    study_df = pd.DataFrame(trial_data, columns=trial_label)
     observation_unit_df = pd.DataFrame(observation_unit_data, columns=observation_unit_label)
     observation_df = pd.DataFrame(observation_data, columns=observation_label)
     factor_df = pd.DataFrame(factor_data, columns=factor_label)
     person_df = pd.DataFrame(person_data, columns=person_label)
-    gps_df = pd.DataFrame(gps_data, columns=gps_label)
 
-    study_df.to_csv("trials.csv", index=False, encoding="utf-8")
-    biological_df.to_csv("biological.csv", index=False, encoding="utf-8")
+    
     observation_unit_df.to_csv("observation_unit.csv", index=False, encoding="utf-8")
     observation_df.to_csv("observation.csv", index=False, encoding="utf-8")
     factor_df.to_csv("factor.csv", index=False, encoding="utf-8")
@@ -341,5 +326,5 @@ if __name__ == '__main__':
     #print(observation_data)
     #print(factor_data)
     print(person_data)
-    print(f"Variable not found => {variable_not_found}")
+    print(f"Variable not found => {variable_not_found}")"""
 
